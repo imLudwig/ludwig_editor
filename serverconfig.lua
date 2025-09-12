d2564e5d2eea19d3b7443fb65c363889dd20e367
Ludwig <luisrahm37@gmail.com>
2025-09-12 13:24:31 +0200

    Update serverconfig.lua

--- serverconfig.lua
+++ serverconfig.lua
@@ -3,10 +3,7 @@ Config = {}
 Config.Debug = true
 
 Config.Permissions = {
-    ["steam:110000102cbb74c"] = true, -- roschy
-    ["steam:1100001057caf82"] = true, -- dusk
-    ["steam:110000113cc23d9"] = true, -- ludwig
-    ["steam:11000010de93587"] = true, -- callahan
+    ["steam:xxxxxxxxxxxxxxxxxxx"] = true, -- ludwig
 }
 
 Config.FilesToEdit = {
