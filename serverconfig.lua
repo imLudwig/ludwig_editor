3630f4c9779f04cafeeb60e928cdeb70cca0e7d0
Ludwig <luisrahm37@gmail.com>
2026-02-23 07:06:18 +0100

    Update serverconfig.lua

--- serverconfig.lua
+++ serverconfig.lua
@@ -9,3 +9,6 @@ Config.Permissions = {
 Config.FilesToEdit = {
     ["Housing Config"] = { script = "bcc-housing", path = "houseconfig.lua", absolutePath = "/fxserver/txData/VORPCore_A149B8.base/resources/[EXTRAS]/[BCC]/bcc-housing/houseconfig.lua" } --pfad aus sicht der ressource, also wie im fxmanifest
 }
+
+-- Falls ich nochmal anfange: 
+-- one resource can safe files into another Resource by add_filesystem_permission resource1 resource2
